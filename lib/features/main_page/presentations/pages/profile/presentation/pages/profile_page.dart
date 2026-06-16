3173d1029fd2febca6e1bf98238a476e751964a5
SegnonDimas <segnondimas@gmail.com>
2026-06-16 12:49:17 +0100

    Quelques applications avec Column et Row

--- lib/features/main_page/presentations/pages/profile/presentation/pages/profile_page.dart
+++ lib/features/main_page/presentations/pages/profile/presentation/pages/profile_page.dart
@@ -12,6 +12,7 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       // AppBar
       appBar: AppBar(
         // leading
@@ -27,20 +28,52 @@ class _ProfileState extends State<Profile> {
       ),
 
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.end,
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(height: 500, width: 10, color: Colors.red,),
-            Container(height: 400, width: 10, color: Colors.green,),
-            Container(height: 300, width: 10, color: Colors.blue,),
-            Container(height: 450, width: 10, color: Colors.red,),
-            Container(height: 100, width: 10, color: Colors.green,),
-            Container(height: 50, width: 10, color: Colors.blue,),
-            Container(height: 600, width: 10, color: Colors.red,),
-            Container(height: 150, width: 10, color: Colors.green,),
-            Container(height: 40, width: 10, color: Colors.blue,),
-          ],
+            Container(height: 150, width: 150,
+                child: Image.network("https://media.istockphoto.com/id/1446465647/fr/vectoriel/avatar-portrait-dun-jeune-gar%C3%A7on-caucasien-homme-en-rond-cadre-illustration-vectorielle.jpg?s=612x612&w=0&k=20&c=ABO-hml56eP4dkThhoVExlI-ymBRidU-AtvPdOZG-mU=")
+                ,),
+            Row
+              (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("+ Add name"),
+                SizedBox(width: 10,),
+                Text("Edit")
+              ],
+            ),
+            SizedBox(height: 10,),
+            Text("@dimassegnon"),
+            SizedBox(height: 20,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("Following"),
+                Text("Followers"),
+                Text("Likes")
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage("https://media.gqmagazine.fr/photos/5e301d9bd6a04300082cc271/4:3/w_5120,h_3840,c_limit/P90233585_highRes_the-bmw-m3-e30-09-20.jpg"),),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(1000),
+                    child: SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Image.network("https://media.istockphoto.com/id/2176502674/fr/photo/mercedes-benz-amg-gt-2018-voiture-de-sport-vue-dangle.jpg?s=612x612&w=0&k=20&c=hnt7YVzZAnLGWgN4gVTw_ds9tTGNtOL00YviU7ENdKA=",
+                        fit: BoxFit.cover,
+                        ))),
+              ],
+            )
+            ],
         ),
       ),
 
